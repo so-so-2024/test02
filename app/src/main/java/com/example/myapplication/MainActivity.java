d5b67d755a07740958f86bbd2f7bd28e6968eb8b
so-so-2024 <118402755+so-so-2024@users.noreply.github.com>
2022-11-18 01:26:10 +0330

    ch1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,10 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate: hooooo");
-        Log.i(TAG, "onCreate: ho88oooo");
+ 
         Log.i(TAG, "onCreate: 999999999999999999");
         Toast.makeText(this, "dfsdfs", Toast.LENGTH_SHORT).show();
         Log.i(TAG, "onCreate: 666");
         Log.i(TAG, "onCreate: hoo555ooo");
+        
+           Log.i(TAG, "onCreate: ho88oooo"); /* add gitub */
     }
 }
